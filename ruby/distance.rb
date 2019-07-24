f2fc261589bfcbc959d701f91b054374353fba8c
tejaswinigowda1996@gmail.com <tejaswinigowda1996@gmail.com>
2019-07-24 11:42:04 +0530

    latest changes on api key

--- ruby/distance.rb
+++ ruby/distance.rb
@@ -3,7 +3,7 @@ require 'certified'
 puts "Enter source and destination"
 s=gets.chomp
 d=gets.chomp
-response=HTTParty.get("https://maps.googleapis.com/maps/api/distancematrix/json?units=imperial&origins=#{s}&destinations=#{d}&key=AIzaSyCyb2UA5qRdTOAvNj0SeJn681NLhU3AcsI")
+response=HTTParty.get("https://maps.googleapis.com/maps/api/distancematrix/json?units=imperial&origins=#{s}&destinations=#{d}&key=yourkey")
 #puts response
 puts " Distance is : #{response["rows"][0]["elements"][0]["distance"]["text"]}"
 puts " Time taken is : #{response["rows"][0]["elements"][0]["duration"]["text"]}"
